ce62059448d8e4b302faf79501b862e330f6cb1f
jmbaik <famian@daum.net>
2023-12-01 02:23:20 +0900

    데이터 커밋

--- src/main/resources/sql/cih_data.sql
+++ src/main/resources/sql/cih_data.sql
@@ -4,3 +4,222 @@ VALUES('u0001', 'famian@daum.net', '01048515568', '1234', 'S', NULL, '백종민'
 INSERT INTO cihdb.tb_admins
 (uid, email, phone, password, admin_level, c_code, name, nickname, comment, pic, token, regid, regdate, updid, upddate)
 VALUES('u0002', 'whitefox@naver.com', '01042149199', '1234', 'S', NULL, '백현정', '', NULL, NULL, NULL, 'admin', '2023-11-25 21:47:02.000', 'admin', '2023-11-25 21:47:02.000');
+
+INSERT INTO tb_origin_vid (channel_id,title,description,regid,regdt,updid,upddt) VALUES
+	 ('UC-JbwIkZI1f3Bings3VO3Sg','온누리교회',NULL,'admin','2023-11-15 20:26:05','u0001','2023-11-30 18:56:11'),
+	 ('UCBh3Qv-rKu6ZDxEcrio70Hw','분당우리교회',NULL,'admin','2023-11-21 17:58:09','u0001','2023-11-30 18:56:51'),
+	 ('UCGrMG52egZF3j0CUv9c5FUA','베이직교회',NULL,'admin','2023-11-15 20:35:12','u0001','2023-11-30 18:57:38'),
+	 ('UCjXMsndMsc538jppd4zqnCg','선한목자교회',NULL,'admin','2023-11-16 01:42:41','u0001','2023-11-30 18:55:36'),
+	 ('UCLo2INtD06KKwxJG5YZbABQ','초신자의 시선',NULL,'u0001','2023-12-01 00:37:45','u0001','2023-12-01 00:38:03'),
+	 ('UCm2kV3p3f2Es2XOohDCrcnA','낮은담교회',NULL,'admin','2023-11-16 01:40:29','u0001','2023-11-30 18:54:46'),
+	 ('UCX9kQaS0ZIeq7sK9tcugYFw','수원하나교회',NULL,'admin','2023-11-15 20:24:21','u0001','2023-11-30 18:53:27');
+
+
+
+INSERT INTO tb_youtube_pastor (vid,p_code,channel_id,title,channel_title,thumbnail_d,thumbnail_m,thumbnail_h,grade,sort,description,create_ymd,publishedAt,regid,regdt,updid,upddt,del_yn) VALUES
+	 ('-Czmoh1nxlg','P000005','UCGrMG52egZF3j0CUv9c5FUA','[LIVE] 마음을 연단하신다 (잠 17:1-12)_아침예배_베이직교회_조정민 목사_20231115','베이직교회','https://i.ytimg.com/vi/-Czmoh1nxlg/default.jpg','https://i.ytimg.com/vi/-Czmoh1nxlg/mqdefault.jpg','https://i.ytimg.com/vi/-Czmoh1nxlg/hqdefault.jpg','C',80,'타임코드를 클릭하시면 해당 영상부터 보실 수 있습니다.
+찬양 5:21
+말씀 11:39
+
+BASIC COMMUNITY CHURCH
+
+아침예배
+베이직교회
+
+http://www.basicchurch.or.kr​
+* 이 채널은 베이직교회 공식 채널입니다.
+* ''베이직교회''와 ''조정민 목사''의 모든 콘텐츠에 대한 저작권은 베이직교회에 있습니다. 따라서 사전 동의가 없이 편집 및 재배포 된 영상과 음성은 별도의 안내 없이 저작권 신고 및 삭제처리 되며, 해당 채널에 패널티가 부여됩니다.','20231130','2023-11-14T22:09:43Z','u0001','2023-11-30 18:57:23','u0001','2023-11-30 18:57:23','N'),
+	 ('0NaLC1r-p2U','P000001','UCjXMsndMsc538jppd4zqnCg','[선한목자교회] 주일 설교 2023년 07월 30일 | 고난의 때 순종을 배우다 - 유기성 원로목사 [히브리서 5:7-10]','선한목자교회','https://i.ytimg.com/vi/0NaLC1r-p2U/default.jpg','https://i.ytimg.com/vi/0NaLC1r-p2U/mqdefault.jpg','https://i.ytimg.com/vi/0NaLC1r-p2U/hqdefault.jpg','C',80,'[선한목자교회] 주일 설교 2023년 07월 30일
+고난의 때 순종을 배우다 - 유기성 원로목사 [히브리서 5:7-10]
+
+#선한목자교회 #김다위목사
+
+교회(church)/단체명 : 선한목자교회(Good Shepherd Methodist Church)
+CCLI License # 413859
+CCLI streaming License #12418
+
+√ 선한목자교회 영상의 저작권은 선한목자교회에 있습니다.
+   선한목자교회의 모든 영상은 개인 소장을 제외한, 임의 편집 및 이윤을 추구하기 위한 온라인에 공유를 엄격히 금합니다.
+
+# 예수동행 미디어플렛폼 위지온티비 바로가기 : https://www.youtube.com/channel/UCAv-udQln-Qx6tGVE9SH5pw
+# 선한목자교회 온라인교회 바로가기 : https://online.gsmch.org/
+# 예수동행일기 site : https://jwj.kr/login/login.do
+# 김다위 목사 페이스북 : https://www.facebook.com/david.dawe.kim
+# 예수동행기도 : http://www.praywithjesus.org/
+# 선한목자교회 homepage : http://www.gsmch.org
+# 예수동행기도 : http://www.praywithjesus.org
+# 김다위 목사 유튜브 Channel : http://youtube.com/@davidkim_gsmch
+# 4부예배 수어중심 통역설교 channel : https://www.youtube.com/channel/UC8DXnLZR9CWYkSOOqdwqMfA','20231130','2023-07-30T07:04:49Z','u0001','2023-11-30 18:55:36','u0001','2023-11-30 18:55:36','N'),
+	 ('0YYLQP8tS7o','P000004','UCX9kQaS0ZIeq7sK9tcugYFw','[이사야 1] 위로하라, 내 백성을 위로하라 (사40:1-11절)│2023. 3. 3 금요예배','수원하나교회','https://i.ytimg.com/vi/0YYLQP8tS7o/default.jpg','https://i.ytimg.com/vi/0YYLQP8tS7o/mqdefault.jpg','https://i.ytimg.com/vi/0YYLQP8tS7o/hqdefault.jpg','C',70,'2023년 3월 3일 금요예배
+설교: 고성준목사
+
+[이사야 1] – 위로하라, 내 백성을 위로하라 (사40:1-11절)','20231130','2023-03-03T16:16:34Z','u0001','2023-11-30 18:53:27','u0001','2023-11-30 18:53:27','N'),
+	 ('2pVqgdZlN-M','P000001','UCjXMsndMsc538jppd4zqnCg','[선한목자교회] 주일설교 2023년 08월 27일 | 마음이 부드러워질 때까지 - 유기성 목사 [에스겔 36:26-27]','선한목자교회','https://i.ytimg.com/vi/2pVqgdZlN-M/default.jpg','https://i.ytimg.com/vi/2pVqgdZlN-M/mqdefault.jpg','https://i.ytimg.com/vi/2pVqgdZlN-M/hqdefault.jpg','B',90,'[선한목자교회] 주일 설교 2023년 08월 27일
+마음이 부드러워질 때까지 - 유기성 목사 [에스겔 36:26-27]
+
+#선한목자교회 #김다위목사
+
+교회(church)/단체명 : 선한목자교회(Good Shepherd Methodist Church)
+CCLI License # 413859
+CCLI streaming License #12418
+
+√ 선한목자교회 영상의 저작권은 선한목자교회에 있습니다.
+   선한목자교회의 모든 영상은 개인 소장을 제외한, 임의 편집 및 이윤을 추구하기 위한 온라인에 공유를 엄격히 금합니다.
+
+# 예수동행 미디어플렛폼 위지온티비 바로가기 : https://www.youtube.com/channel/UCAv-udQln-Qx6tGVE9SH5pw
+# 선한목자교회 온라인교회 바로가기 : https://online.gsmch.org/
+# 예수동행일기 site : https://jwj.kr/login/login.do
+# 김다위 목사 페이스북 : https://www.facebook.com/david.dawe.kim
+# 예수동행기도 : http://www.praywithjesus.org/
+# 선한목자교회 homepage : http://www.gsmch.org
+# 예수동행기도 : http://www.praywithjesus.org
+# 김다위 목사 유튜브 Channel : http://youtube.com/@davidkim_gsmch
+# 4부예배 수어중심 통역설교 channel : https://www.youtube.com/channel/UC8DXnLZR9CWYkSOOqdwqMfA','20231130','2023-08-27T07:32:30Z','u0001','2023-11-30 18:55:19','u0001','2023-11-30 18:55:19','N'),
+	 ('CX7n7U_ncdM','P000005','UCGrMG52egZF3j0CUv9c5FUA','[왜 예수인가?] 1강_MetaReligion(예수님은 종교가 아니다)_조정민 목사_130904','베이직교회','https://i.ytimg.com/vi/CX7n7U_ncdM/default.jpg','https://i.ytimg.com/vi/CX7n7U_ncdM/mqdefault.jpg','https://i.ytimg.com/vi/CX7n7U_ncdM/hqdefault.jpg','C',70,'BASIC COMMUNITY CHURCH
+
+베이직교회 수요강좌
+일시 : 2013년 9월 4일
+[왜 예수인가?] 1강_MetaReligion(예수님은 종교가 아니다)_조정민 목사
+
+http://www.basicchurch.or.kr
+* 이 채널은 베이직교회 공식 채널입니다.
+* 베이직교회는 설교영상에 광고를 넣지 않습니다.
+* ''베이직교회''와 ''조정민 목사''의 모든 콘텐츠에 대한 저작권은 베이직교회에 있습니다. 따라서 사전 동의가 없이 편집 및 재배포 된 영상과 음성은 별도의 안내 없이 저작권 신고 및 삭제처리 되며, 해당 채널에 패널티가 부여됩니다.','20231130','2020-11-24T02:29:13Z','u0001','2023-11-30 18:57:38','u0001','2023-11-30 18:57:38','N'),
+	 ('K7d2QYftRII','P000006','UCBh3Qv-rKu6ZDxEcrio70Hw','2023-11-19 | 은혜에 대한 감사가 흐르게 하라 | 이찬수 목사 | 분당우리교회 주일설교','분당우리교회','https://i.ytimg.com/vi/K7d2QYftRII/default.jpg','https://i.ytimg.com/vi/K7d2QYftRII/mqdefault.jpg','https://i.ytimg.com/vi/K7d2QYftRII/hqdefault.jpg','C',80,'2023-11-19
+분당우리교회 주일설교
+은혜에 대한 감사가 흐르게 하라 (고린도전서 15장 9-10절)
+이찬수 목사
+
+#분당우리교회, #분당우리교회주일설교, #분당우리교회이찬수목사
+
+▒▒▒　유튜브 영상 중 광고 안내　▒▒▒
+본 영상에 광고가 나올 경우 광고는 분당우리교회와 아무 연관이 없습니다.
+영상에 사용된 찬양 저작권자의 수익 활동이나 유튜브 자체 광고입니다.
+
+▒▒▒　분당우리교회 홈페이지 안내　▒▒▒
+- 분당우리교회 홈페이지 : http://www.woorichurch.org
+- 분당우리교회 방송 홈페이지 : http://tv.woorichurch.org
+- 분당우리교회 카카오톡 : https://bit.ly/woorikakao
+ (카카오톡 채널을 추가하시면 관련 소식을 받으실 수 있습니다)','20231130','2023-11-19T07:28:35Z','u0001','2023-11-30 18:56:51','u0001','2023-11-30 18:56:51','N'),
+	 ('KJZ_BFRMtXw','P000003','UC-JbwIkZI1f3Bings3VO3Sg','[이재훈 위임목사] 깨어지는 가정, 깨지지 않는 언약 (창세기 27:18-29)│2023.11.05','온누리교회','https://i.ytimg.com/vi/KJZ_BFRMtXw/default.jpg','https://i.ytimg.com/vi/KJZ_BFRMtXw/mqdefault.jpg','https://i.ytimg.com/vi/KJZ_BFRMtXw/hqdefault.jpg','C',80,'2023.11.05(일)
+온누리교회 일요주일 2부 예배
+
+깨어지는 가정, 깨지지 않는 언약 (창세기 27:18-29)
+____________________________________________________________
+
+▷ 온누리교회 홈페이지                  : http://www.onnuri.org/
+▷ 온누리교회 인스타그램              : https://www.instagram.com/onnuri.acts29/
+▷ 온누리교회 페이스북                  : https://www.facebook.com/onnuriCommunityChurch
+▷ 온누리교회 유튜브 구독하기      : https://www.youtube.com/channel/UC-JbwIkZI1f3Bings3VO3Sg?sub_confirmation=1
+▷ CGNTV 유튜브                            : https://www.youtube.com/channel/UCB8GtWm-5xKmIeh9ssIDyBA
+▷ CGNTV SOON 유튜브                 : https://www.youtube.com/channel/UCMqsPR3RFYP2L7iLeMQhUDQ','20231130','2023-11-05T07:04:43Z','u0001','2023-11-30 18:55:54','u0001','2023-11-30 18:55:54','N'),
+	 ('os_Srs6aRMY','P000002','UCm2kV3p3f2Es2XOohDCrcnA','김관성 목사  낮은담교회&행신교회 연합예배   2023. 11. 5 “음부의 권세를 이기는 교회”  마태복음16:18','낮은담교회','https://i.ytimg.com/vi/os_Srs6aRMY/default.jpg','https://i.ytimg.com/vi/os_Srs6aRMY/mqdefault.jpg','https://i.ytimg.com/vi/os_Srs6aRMY/hqdefault.jpg','C',70,'#김관성목사 #낮은담침례교회 #낮은담교회 #라이트하우스울산 #주일예배  #실시간아침예배 #대예배
+
+마태복음 16:18
+
+18. 또 내가 네게 이르노니 너는 베드로라 내가 이 반석
+      위에 내 교회를 세우리니 음부의 권세가 이기지 못하리라
+
+
+기독교한국침례회 낮은담교회(라이트하우스울산)  담임목사 김관성
+주소 : 울산광역시 남구 돋질로 374번길 9
+홈페이지 : http://www.lowwall.org
+인스타그램 : https://www.instagram.com/lowwallchurch
+페이스북 : https://www.facebook.com/lowwallchurch
+
+- 예배시간안내 -
+주일예배 오전 11시 30분
+수요예배 오전 10시 30분
+금요기도회 저녁 8시
+새벽기도(월-금, 온라인) 새벽 6시
+
+헌금 및 후원계좌 : 농협  351-1238-5776-83(낮은담침례교회)
+                                 페이팔 http://PayPal.Me/lowwallchurch
+
+저희 낮은담교회는 한국교회저작권협회를 통해 한국음악저작권협회 (KOMCA)와 CCLI로부터 교회저작권라이선스와 스트리밍 라이선스를 취득하였습니다.
+낮은담교회는 한국음악저작권협회 (KOMCA)와 CCLI 교회 저작권 라이런스 및 스트리밍 라이선스 규정에 따라 비영리 예배 목적으로 이용합니다.
+본 영상에 광고가 재생될 경우, 본 교회가 아닌 노래의 저작권자가 결정한 것으로 수익은 교회가 아닌 노래의 저작권자에게 분배됩니다.
+
+낮은담교회의 라이선스 취득 사실을 한국음악저작권협회(KOMCA)와 CCLI 에서 확인할 수 있습니다.
+
+“낮은담교회”
+CCLI Licence #755232
+CCLI Streaming License #2366411','20231130','2023-11-05T16:11:55Z','u0001','2023-11-30 18:53:50','u0001','2023-11-30 18:53:50','N'),
+	 ('POVvdYxZUck','P000001','UCjXMsndMsc538jppd4zqnCg','[선한목자교회] 주일설교 2023년 07월 16일 | 전능자의 날개 아래 거하라 - 유기성 원로목사 [시편 91:1-16]','선한목자교회','https://i.ytimg.com/vi/POVvdYxZUck/default.jpg','https://i.ytimg.com/vi/POVvdYxZUck/mqdefault.jpg','https://i.ytimg.com/vi/POVvdYxZUck/hqdefault.jpg','C',80,'[선한목자교회] 주일설교 2023년 07월 16일
+전능자의 날개 아래 거하라 - 유기성 원로목사 [시편 91:1-16]
+
+#선한목자교회 #김다위목사
+
+교회(church)/단체명 : 선한목자교회(Good Shepherd Methodist Church)
+CCLI License # 413859
+CCLI streaming License #12418
+
+√ 선한목자교회 영상의 저작권은 선한목자교회에 있습니다.
+   선한목자교회의 모든 영상은 개인 소장을 제외한, 임의 편집 및 이윤을 추구하기 위한 온라인에 공유를 엄격히 금합니다.
+
+# 예수동행 미디어플렛폼 위지온티비 바로가기 : https://www.youtube.com/channel/UCAv-udQln-Qx6tGVE9SH5pw
+# 선한목자교회 온라인교회 바로가기 : https://online.gsmch.org/
+# 예수동행일기 site : https://jwj.kr/login/login.do
+# 김다위 목사 페이스북 : https://www.facebook.com/david.dawe.kim
+# 예수동행기도 : http://www.praywithjesus.org/
+# 선한목자교회 homepage : http://www.gsmch.org
+# 예수동행기도 : http://www.praywithjesus.org
+# 김다위 목사 유튜브 Channel : http://youtube.com/@davidkim_gsmch
+# 4부예배 수어중심 통역설교 channel : https://www.youtube.com/channel/UC8DXnLZR9CWYkSOOqdwqMfA','20231130','2023-07-16T06:52:17Z','u0001','2023-11-30 18:55:02','u0001','2023-11-30 18:55:02','N'),
+	 ('T5XZxLdIb3s','P000002','UCm2kV3p3f2Es2XOohDCrcnA','김관성 목사  낮은담교회 주일예배   2023. 7. 23  우리들의 가장 큰 문제, 말과 언어   전도서 5:4-6','낮은담교회','https://i.ytimg.com/vi/T5XZxLdIb3s/default.jpg','https://i.ytimg.com/vi/T5XZxLdIb3s/mqdefault.jpg','https://i.ytimg.com/vi/T5XZxLdIb3s/hqdefault.jpg','C',70,'#김관성목사 #낮은담침례교회 #낮은담교회 #라이트하우스울산 #주일예배  #실시간예배 #주일낮예배 #대예배
+
+
+전도서 5:4-6
+
+4. 네가 하나님께 서원하였거든 갚기를
+    더디게 하지 말라 하나님은 우매한 자들을
+    기뻐하지 아니하시나니 서원한 것을 갚으라
+5. 서원하고 갚지 아니하는 것보다 서원하지
+    아니하는 것이 더 나으니
+6. 네 입으로 네 육체가 범죄하게 하지 말라 사자
+    앞에서 내가 서원한 것이 실수라고 말하지 말라
+    어찌 하나님께서 네 목소리로 말미암아 진노하사
+    네 손으로 한 것을 멸하시게 하랴
+
+
+기독교한국침례회 낮은담교회(라이트하우스울산)  담임목사 김관성
+주소 : 울산광역시 남구 돋질로 374번길 9
+홈페이지 : http://www.lowwall.org
+인스타그램 : https://www.instagram.com/lowwallchurch
+페이스북 : https://www.facebook.com/lowwallchurch
+
+- 예배시간안내 -
+주일예배 오전 11시 30분
+수요예배 오전 10시 30분
+금요기도회 저녁 8시
+새벽기도(월-금, 온라인) 새벽 6시
+
+헌금 및 후원계좌 : 농협  351-1238-5776-83(낮은담침례교회)
+                                 페이팔 http://PayPal.Me/lowwallchurch
+
+저희 낮은담교회는 한국교회저작권협회를 통해 한국음악저작권협회 (KOMCA)와 CCLI로부터 교회저작권라이선스와 스트리밍 라이선스를 취득하였습니다.
+낮은담교회는 한국음악저작권협회 (KOMCA)와 CCLI 교회 저작권 라이런스 및 스트리밍 라이선스 규정에 따라 비영리 예배 목적으로 이용합니다.
+본 영상에 광고가 재생될 경우, 본 교회가 아닌 노래의 저작권자가 결정한 것으로 수익은 교회가 아닌 노래의 저작권자에게 분배됩니다.
+
+낮은담교회의 라이선스 취득 사실을 한국음악저작권협회(KOMCA)와 CCLI 에서 확인할 수 있습니다.
+
+“낮은담교회”
+CCLI Licence #755232
+CCLI Streaming License #2366411','20231130','2023-07-23T04:31:00Z','u0001','2023-11-30 18:54:46','u0001','2023-11-30 18:54:46','N');
+INSERT INTO tb_youtube_pastor (vid,p_code,channel_id,title,channel_title,thumbnail_d,thumbnail_m,thumbnail_h,grade,sort,description,create_ymd,publishedAt,regid,regdt,updid,upddt,del_yn) VALUES
+	 ('tZ-2983_FxY','P000003','UC-JbwIkZI1f3Bings3VO3Sg','[이재훈 담임목사] 모든 것의 시작 (창세기 1:1-2)│2023.05.07(일)','온누리교회','https://i.ytimg.com/vi/tZ-2983_FxY/default.jpg','https://i.ytimg.com/vi/tZ-2983_FxY/mqdefault.jpg','https://i.ytimg.com/vi/tZ-2983_FxY/hqdefault.jpg','C',80,'2023.05.07(일)
+온누리교회 일요주일 3부 예배
+
+모든 것의 시작 (창세기 1:1-2)
+____________________________________________________________
+
+▷ 온누리교회 홈페이지                  : http://www.onnuri.org/
+▷ 온누리교회 인스타그램              : https://www.instagram.com/onnuri.acts29/
+▷ 온누리교회 페이스북                  : https://www.facebook.com/onnuriCommunityChurch
+▷ 온누리교회 유튜브 구독하기      : https://www.youtube.com/channel/UC-JbwIkZI1f3Bings3VO3Sg?sub_confirmation=1
+▷ CGNTV 유튜브                            : https://www.youtube.com/channel/UCB8GtWm-5xKmIeh9ssIDyBA
+▷ CGNTV SOON 유튜브                 : https://www.youtube.com/channel/UCMqsPR3RFYP2L7iLeMQhUDQ','20231130','2023-05-07T11:51:06Z','u0001','2023-11-30 18:56:11','u0001','2023-11-30 18:56:11','N'),
+	 ('xUgWctrye4w','P000004','UCX9kQaS0ZIeq7sK9tcugYFw','[요한복음]01. New Start (사43:18-19절, 요1:14절)│2023.1.1 주일예배','수원하나교회','https://i.ytimg.com/vi/xUgWctrye4w/default.jpg','https://i.ytimg.com/vi/xUgWctrye4w/mqdefault.jpg','https://i.ytimg.com/vi/xUgWctrye4w/hqdefault.jpg','C',80,'2023년 1월 1일 주일예배
+설교: 고성준목사
+
+[요한복음1 – New Start (사43:18-19절, 요1:14절)]','20231130','2023-01-02T01:55:59Z','u0001','2023-11-30 18:52:46','u0001','2023-11-30 18:52:46','N');
