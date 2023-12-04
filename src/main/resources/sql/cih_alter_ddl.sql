394aed7bc6c385ef269457df85c9501d9067814b
jmbaik <famian@daum.net>
2023-12-04 11:30:30 +0900

    서치 api 수정

--- src/main/resources/sql/cih_alter_ddl.sql
+++ src/main/resources/sql/cih_alter_ddl.sql
@@ -1,67 +1,17 @@
-ALTER TABLE cihdb.tb_youtube_pastor  ADD prevPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_pastor CHANGE prevPageToken prevPageToken INT NULL AFTER publishedAt;
-ALTER TABLE cihdb.tb_youtube_pastor ADD nextPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_pastor CHANGE nextPageToken nextPageToken INT NULL AFTER prevPageToken;
-ALTER TABLE cihdb.tb_youtube_pastor ADD totalResults INT NULL;
-ALTER TABLE cihdb.tb_youtube_pastor CHANGE totalResults totalResults INT NULL AFTER nextPageToken;
-ALTER TABLE cihdb.tb_youtube_pastor ADD itemNo INT NULL;
-ALTER TABLE cihdb.tb_youtube_pastor CHANGE itemNo itemNo INT NULL AFTER totalResults;
+ALTER TABLE cihdb.tb_youtube_sermon ADD p_code varchar(7) NULL;
+ALTER TABLE cihdb.tb_youtube_sermon CHANGE p_code p_code varchar(7) NULL AFTER thumbnail_d;
 
-ALTER TABLE cihdb.tb_youtube_celeb  ADD prevPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_celeb CHANGE prevPageToken prevPageToken INT NULL AFTER publishedAt;
-ALTER TABLE cihdb.tb_youtube_celeb ADD nextPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_celeb CHANGE nextPageToken nextPageToken INT NULL AFTER prevPageToken;
-ALTER TABLE cihdb.tb_youtube_celeb ADD totalResults INT NULL;
-ALTER TABLE cihdb.tb_youtube_celeb CHANGE totalResults totalResults INT NULL AFTER nextPageToken;
-ALTER TABLE cihdb.tb_youtube_celeb ADD itemNo INT NULL;
-ALTER TABLE cihdb.tb_youtube_celeb CHANGE itemNo itemNo INT NULL AFTER totalResults;
+ALTER TABLE cihdb.tb_youtube_sermon MODIFY COLUMN prevPageToken VARCHAR(200) DEFAULT NULL NULL;
+ALTER TABLE cihdb.tb_youtube_sermon MODIFY COLUMN nextPageToken VARCHAR(200) DEFAULT NULL NULL;
 
-ALTER TABLE cihdb.tb_youtube_sermon  ADD prevPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_sermon CHANGE prevPageToken prevPageToken INT NULL AFTER publishedAt;
-ALTER TABLE cihdb.tb_youtube_sermon ADD nextPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_sermon CHANGE nextPageToken nextPageToken INT NULL AFTER prevPageToken;
-ALTER TABLE cihdb.tb_youtube_sermon ADD totalResults INT NULL;
-ALTER TABLE cihdb.tb_youtube_sermon CHANGE totalResults totalResults INT NULL AFTER nextPageToken;
-ALTER TABLE cihdb.tb_youtube_sermon ADD itemNo INT NULL;
-ALTER TABLE cihdb.tb_youtube_sermon CHANGE itemNo itemNo INT NULL AFTER totalResults;
 
-ALTER TABLE cihdb.tb_youtube_mercy ADD prevPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_mercy CHANGE prevPageToken prevPageToken INT NULL AFTER publishedAt;
-ALTER TABLE cihdb.tb_youtube_mercy ADD nextPageToken INT NULL;
-ALTER TABLE cihdb.tb_youtube_mercy CHANGE nextPageToken nextPageToken INT NULL AFTER prevPageToken;
-ALTER TABLE cihdb.tb_youtube_mercy ADD totalResults INT NULL;
-ALTER TABLE cihdb.tb_youtube_mercy CHANGE totalResults totalResults INT NULL AFTER nextPageToken;
-ALTER TABLE cihdb.tb_youtube_mercy ADD itemNo INT NULL;
-ALTER TABLE cihdb.tb_youtube_mercy CHANGE itemNo itemNo INT NULL AFTER totalResults;
+ALTER TABLE cihdb.tb_youtube_mercy  MODIFY COLUMN prevPageToken VARCHAR(200) DEFAULT NULL NULL;
+ALTER TABLE cihdb.tb_youtube_mercy MODIFY COLUMN nextPageToken VARCHAR(200) DEFAULT NULL NULL;
 
-ALTER TABLE cihdb.tb_shorts_ccm  ADD prevPageToken INT NULL;
-ALTER TABLE cihdb.tb_shorts_ccm CHANGE prevPageToken prevPageToken INT NULL AFTER publishedAt;
-ALTER TABLE cihdb.tb_shorts_ccm ADD nextPageToken INT NULL;
-ALTER TABLE cihdb.tb_shorts_ccm CHANGE nextPageToken nextPageToken INT NULL AFTER prevPageToken;
-ALTER TABLE cihdb.tb_shorts_ccm ADD totalResults INT NULL;
-ALTER TABLE cihdb.tb_shorts_ccm CHANGE totalResults totalResults INT NULL AFTER nextPageToken;
-ALTER TABLE cihdb.tb_shorts_ccm ADD itemNo INT NULL;
-ALTER TABLE cihdb.tb_shorts_ccm CHANGE itemNo itemNo INT NULL AFTER totalResults;
 
-ALTER TABLE cihdb.tb_youtube_pastor ADD qsignal varchar(200) NULL;
-ALTER TABLE cihdb.tb_youtube_pastor CHANGE qsignal qsignal varchar(200) NULL AFTER itemNo;
+ALTER TABLE cihdb.tb_youtube_celeb MODIFY COLUMN prevPageToken VARCHAR(200) DEFAULT NULL NULL;
+ALTER TABLE cihdb.tb_youtube_celeb MODIFY COLUMN nextPageToken VARCHAR(200) DEFAULT NULL NULL;
 
-ALTER TABLE cihdb.tb_youtube_sermon ADD qsignal varchar(200) NULL;
-ALTER TABLE cihdb.tb_youtube_sermon CHANGE qsignal qsignal varchar(200) NULL AFTER itemNo;
+ALTER TABLE cihdb.tb_shorts_ccm  MODIFY COLUMN prevPageToken VARCHAR(200) DEFAULT NULL NULL;
+ALTER TABLE cihdb.tb_shorts_ccm  MODIFY COLUMN nextPageToken VARCHAR(200) DEFAULT NULL NULL;
 
-ALTER TABLE cihdb.tb_youtube_celeb ADD qsignal varchar(200) NULL;
-ALTER TABLE cihdb.tb_youtube_celeb CHANGE qsignal qsignal varchar(200) NULL AFTER itemNo;
-
-ALTER TABLE cihdb.tb_youtube_mercy ADD qsignal varchar(200) NULL;
-ALTER TABLE cihdb.tb_youtube_mercy CHANGE qsignal qsignal varchar(200) NULL AFTER itemNo;
-
-ALTER TABLE cihdb.tb_shorts_ccm  ADD qsignal varchar(200) NULL;
-ALTER TABLE cihdb.tb_shorts_ccm  CHANGE qsignal qsignal varchar(200) NULL AFTER itemNo;
-
-ALTER TABLE cihdb.tb_youtube_pastor CHANGE qsignal qSignal varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
-ALTER TABLE cihdb.tb_youtube_celeb  CHANGE qsignal qSignal varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
-ALTER TABLE cihdb.tb_youtube_sermon  CHANGE qsignal qSignal varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
-ALTER TABLE cihdb.tb_youtube_mercy  CHANGE qsignal qSignal varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
-ALTER TABLE cihdb.tb_shorts_ccm  CHANGE qsignal qSignal varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
-
-commit;
